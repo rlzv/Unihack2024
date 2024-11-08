1ec1fc829d21ec784429120244833cb7b8dadd89
Uni Prowd <unihack235@gmai.com>
2024-11-08 19:45:37 +0200

    meniu

--- Front-End/src/AppBar.tsx
+++ Front-End/src/AppBar.tsx
@@ -54,6 +54,11 @@ const NAVIGATION: Navigation = [
     title: 'Integrations',
     icon: <LayersIcon />,
   },
+  {
+    segment: 'map', // Adaugă un segment pentru hartă
+    title: 'Map',
+    icon: <LayersIcon />, // Poți folosi orice iconiță dorești
+  },
 ];
 
 const demoTheme = extendTheme({
@@ -95,6 +100,7 @@ export default function DashboardLayoutBasic(props: any) {
   const { window } = props;
 
   const router = useDemoRouter('/dashboard');
+  const { pathname } = router;
 
   // Remove this const when copying and pasting into your project.
   const demoWindow = window ? window() : undefined;
@@ -102,47 +108,38 @@ export default function DashboardLayoutBasic(props: any) {
   return (
     <AppProvider
       navigation={NAVIGATION}
+      branding={{
+        logo: <img src="../img/logo_transparent.png" alt="VisitTM logo" />,
+        title: '',
+      }}
       router={router}
       theme={demoTheme}
       window={demoWindow}
     >
+              <DashboardLayout>
+        <PageContainer>
+          {pathname === '/map' && (
+            <iframe
+              title="Google Map"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d387190.2799181496!2d-74.25987571760744!3d40.69767006358627!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c259af18b60165%3A0x8b621f8a7a7d28a4!2sNew%20York%2C%20NY%2C%20USA!5e0!3m2!1sen!2s!4v1633452834502!5m2!1sen!2s"
+              style={{ flex: 1, border: 0 }}
+              allowFullScreen
+              loading="lazy"
+            />
+          )}
+        </PageContainer>
+      </DashboardLayout>
       <DashboardLayout>
         <PageContainer>
-          <Grid container spacing={1}>
-            <Grid size={5} />
-            <Grid size={12}>
-              <Skeleton height={14} />
-            </Grid>
-            <Grid size={12}>
-              <Skeleton height={14} />
-            </Grid>
-            <Grid size={4}>
-              <Skeleton height={100} />
-            </Grid>
-            <Grid size={8}>
-              <Skeleton height={100} />
-            </Grid>
-
-            <Grid size={12}>
-              <Skeleton height={150} />
-            </Grid>
-            <Grid size={12}>
-              <Skeleton height={14} />
-            </Grid>
-
-            <Grid size={3}>
-              <Skeleton height={100} />
-            </Grid>
-            <Grid size={3}>
-              <Skeleton height={100} />
-            </Grid>
-            <Grid size={3}>
-              <Skeleton height={100} />
-            </Grid>
-            <Grid size={3}>
-              <Skeleton height={100} />
-            </Grid>
-          </Grid>
+          {pathname === '/map' && (
+            <iframe
+              title="Google Map"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d387190.2799181496!2d-74.25987571760744!3d40.69767006358627!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c259af18b60165%3A0x8b621f8a7a7d28a4!2sNew%20York%2C%20NY%2C%20USA!5e0!3m2!1sen!2s!4v1633452834502!5m2!1sen!2s"
+              style={{ flex: 1, border: 0 }}
+              allowFullScreen
+              loading="lazy"
+            />
+          )}
         </PageContainer>
       </DashboardLayout>
     </AppProvider>
